e256b07c0652ca9d59ee2dbf752004e3acb8bc88
HarshadGangarde <gangardeharshad2005@gmail.com>
2026-06-14 14:52:37 +0530

    Add files via upload

---
+++ 83ListDynamicInput1.py
@@ -0,0 +1,19 @@
+def main():
+    Size = 0 
+    Value = 0
+
+    print("Enter the Number of Elements : ")
+    Size = int(input())
+
+    Data = list()
+
+    print("Enter the elements :")
+
+    for i in range(Size):
+        Value = int(input())
+        Data.append(Value)
+
+    print(Data)
+    
+if __name__ == "__main__":
+    main()
